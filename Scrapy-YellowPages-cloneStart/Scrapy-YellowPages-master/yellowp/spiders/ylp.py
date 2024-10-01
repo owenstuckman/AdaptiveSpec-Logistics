58020f31ab87e0bafbefd725a266bb76db030431
owenstuckman <ostuckman@outlook.com>
2024-09-30 20:14:41 -0400

    making sure i am safe

--- Scrapy-YellowPages-cloneStart/Scrapy-YellowPages-master/yellowp/spiders/ylp.py
+++ Scrapy-YellowPages-cloneStart/Scrapy-YellowPages-master/yellowp/spiders/ylp.py
@@ -3,19 +3,26 @@ import scrapy
 
 class YlpSpider(scrapy.Spider):
     name = "ylp"
-    allowed_domains = ["www.yellowpages.com"]
+    allowed_domains = ["yellowpages.com"]
     start_urls = ['http://www.yellowpages.com/search?search_terms=Translation&geo_location_terms=Virginia+Beach%2C+VA']
     
-    
     def parse(self, response):
-		companies = response.xpath('//*[@class="info"]')
-		
-		for company in companies:
-			name = company.xpath('h3/a/span[@itemprop="name"]/text()').extract_first()
-			phone = company.xpath('div/div[@class="phones phone primary"]/text()').extract_first()
-			website = company.xpath('div/div[@class="links"]/a/@href').extract_first()
-			yield{'Name':name,'Phone':phone, 'Website':website}
-			
-			
-			
-			
+        # Select each company listing based on the class "info"
+        companies = response.xpath('//div[@class="info"]')
+        
+        for company in companies:
+            # Extract the company name
+            name = company.xpath('.//h2[@class="n"]/a/span[@itemprop="name"]/text()').extract_first()
+            
+            # Extract the company phone number
+            phone = company.xpath('.//div[@class="phones phone primary"]/text()').extract_first()
+            
+            # Extract the company website link
+            website = company.xpath('.//div[@class="links"]/a/@href').extract_first()
+            
+            # Yield a dictionary containing the extracted data
+            yield {
+                'Name': name,
+                'Phone': phone,
+                'Website': website
+            }
