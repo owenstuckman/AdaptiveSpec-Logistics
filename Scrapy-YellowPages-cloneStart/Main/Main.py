8738e6cdbfcabaa3c683087d4d1e8a332bf0b57a
owenstuckman <ostuckman@outlook.com>
2024-09-30 22:48:18 -0400

    working on filtering

--- Scrapy-YellowPages-cloneStart/Main/Main.py
+++ Scrapy-YellowPages-cloneStart/Main/Main.py
@@ -8,14 +8,32 @@ if __name__ == "__main__":
     script_path = os.path.abspath('Scrapy-YellowPages-cloneStart/Scrapy-YellowPages-master/yellowp/spiders/ylp.py')
     
     # Run the script as a separate process
-    subprocess.run(["python", script_path, "Plumbers", "Blacksburg"]) 
+
+    professionString = "Plumbers"
+    locationString = "Colombus"
+
+    # need to input for the parameters
+    subprocess.run(["python", script_path, professionString, locationString]) 
 
     print("done with subprocess")
 
     df = pd.read_csv("Scrapy-YellowPages-cloneStart/Main/output.csv")
+
+
+    # Ensure has basic column data i am looking for 
     filtered_df = df.dropna(subset=['Name', 'Phone', 'Website'])
-    filtered_df.to_csv('Scrapy-YellowPages-cloneStart/Main/filtered_data.csv', index=False)
+
+    filtered_df = df[df['Locality'].str.contains(locationString, case=False, na=False)]
+
+    # filters to optimize selection
+    filtered_df = df[df['TonsOfInfo'].str.contains("Business", case=False, na=False)]
 
     
+
+
+    # create csv file 
+    filtered_df.to_csv('Scrapy-YellowPages-cloneStart/Main/filtered_data.csv', index=False)
+
+    # remove outpud from web scrape
     os.remove("Scrapy-YellowPages-cloneStart/Main/output.csv")
 
