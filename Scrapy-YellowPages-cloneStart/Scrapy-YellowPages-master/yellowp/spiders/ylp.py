cb48f2beabf4322e1f078a616ead8024f9c2441f
owenstuckman <ostuckman@outlook.com>
2024-09-30 20:29:18 -0400

    works but for name it seems to pull a random one off of the page

--- Scrapy-YellowPages-cloneStart/Scrapy-YellowPages-master/yellowp/spiders/ylp.py
+++ Scrapy-YellowPages-cloneStart/Scrapy-YellowPages-master/yellowp/spiders/ylp.py
@@ -10,9 +10,10 @@ class YlpSpider(scrapy.Spider):
         # Select each company listing based on the class "info"
         companies = response.xpath('//div[@class="info"]')
         
+		# /html/body/div[1]/div[2]/div[1]/div[1]/div[4]/div[2]/div[3]/div/div/div[2]/div[1]/h2/a/span
         for company in companies:
             # Extract the company name
-            name = company.xpath('.//div[@class="n"]/a/div[@class="business-name"]/a/span/text()').extract_first()
+            name = company.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[4]/div[2]/div[3]/div/div/div[2]/div[1]/h2/a/span/text()').extract_first()
             
             # Extract the company phone number
             phone = company.xpath('.//div[@class="phones phone primary"]/text()').extract_first()
